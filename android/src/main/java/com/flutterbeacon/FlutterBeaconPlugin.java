e267c9dfec86e0e129b0cad69f464e72759c1ab0
Austyn11 <bachloveryiy@gmail.com>
2023-09-08 15:21:05 +0900

    fix intent flag

--- android/src/main/java/com/flutterbeacon/FlutterBeaconPlugin.java
+++ android/src/main/java/com/flutterbeacon/FlutterBeaconPlugin.java
@@ -166,9 +166,9 @@ public class FlutterBeaconPlugin implements FlutterPlugin, ActivityAware, Method
       if (beaconManager != null && !beaconManager.isBound(beaconScanner.beaconConsumer)) {
 
 //         인텐트로 앱 실행시키면 플러터에서 init부터 다시 시작해서...
-//        Intent mainIntent = new Intent(flutterPluginBinding.getApplicationContext().getPackageName() + ".action");
-        Intent mainIntent = new Intent(flutterPluginBinding.getApplicationContext().getPackageManager().getLaunchIntentForPackage(flutterPluginBinding.getApplicationContext().getPackageName()));
-        mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK
+        Intent mainIntent = new Intent(flutterPluginBinding.getApplicationContext().getPackageName() + ".action");
+//        Intent mainIntent = new Intent(flutterPluginBinding.getApplicationContext().getPackageManager().getLaunchIntentForPackage(flutterPluginBinding.getApplicationContext().getPackageName()));
+//        mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK); // Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TASK
         PendingIntent mainPendingIntent = PendingIntent.getActivity(flutterPluginBinding.getApplicationContext() , 0, mainIntent, PendingIntent.FLAG_ONE_SHOT); // PendingIntent.FLAG_IMMUTABLE
 //        PendingIntent connectionClosePendingIntent = PendingIntent.getS(flutterPluginBinding.getApplicationContext(), 0, mainIntent, PendingIntent.FLAG_ONE_SHOT);
 
